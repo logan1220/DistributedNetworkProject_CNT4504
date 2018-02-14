a6c54d8143bc1634b9d0289bf600526eea602517
Logan <lsirdevan@marketingarmor.com>
2018-02-14 17:00:14 -0500

    Add displayOutput method

--- src/project1cnt/Project1cnt.java
+++ src/project1cnt/Project1cnt.java
@@ -8,7 +8,7 @@ public class Project1cnt {
     public static Scanner scan;
    
     public static void main(String[] args) throws IOException {
-        	
+        	 
         scan = new Scanner(System.in);
         System.out.print("Group 5\n\n");
         System.out.print("Wesley Tucker | Reggie Jackson | Logan Sirdevan\nChloe Cruz | Madison Gourde | Wafaa Safar\n\n");
@@ -24,12 +24,13 @@ public class Project1cnt {
 
     }//end main
 	
-    public static void userChoice (String choice){
+    public static void userChoice (String choice) throws IOException {
 		
 		switch (choice){
                     
-		case"1": 		
-			break;
+		case"1":
+                    displayOutput("date");
+                    break;
 		case"2":
 			break;
 		case"3":
@@ -41,6 +42,7 @@ public class Project1cnt {
 		case"6":
 			break;
 		case"7":
+                        System.exit(0);
 			break;
 		default:
 			System.out.print("Invalid choice! Enter a number 1-7: ");
@@ -48,5 +50,22 @@ public class Project1cnt {
 		}//end switch
                 
 	}//end userChoice
+    
+    public static void displayOutput(String cmd) throws IOException {
+        Process proc = Runtime.getRuntime().exec(cmd);
+        String s = null;
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+
+        // read the output from the command
+        while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+        }
+
+        // read any errors from the attempted command
+        while ((s = stdError.readLine()) != null) {
+            System.out.println(s);
+        }
+    }
     
 }//end project1cnt
